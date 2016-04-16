00285bdbc11760cf16722cd67250e8574e936aa6
Aldo Anizio Lugão Camacho <aldoanizio@yahoo.com.br>
2016-04-16 16:05:00 -0300

    Correção na validação do cliente/grupo ao finalizar pedido sem cadastro

--- upload/catalog/model/total/taxa_por_tipo_pagamento.php
+++ upload/catalog/model/total/taxa_por_tipo_pagamento.php
@@ -28,31 +28,31 @@ class ModelTotalTaxaPorTipoPagamento extends Model
      * @return  array
      */
 
-	public function getTotal(&$total_data, &$total, &$taxes)
+    public function getTotal(&$total_data, &$total, &$taxes)
     {
         // Se o Módulo estiver Ativo (Configuracao no Admin)
 
-		if($this->config->get('taxa_por_tipo_pagamento_status'))
+        if($this->config->get('taxa_por_tipo_pagamento_status'))
         {
             // Array de meios de pagamento configurados no admin
 
-			$metodos_pagamento_array = $this->config->get('taxa_por_tipo_pagamento_metodos');
+            $metodos_pagamento_array = $this->config->get('taxa_por_tipo_pagamento_metodos');
 
             // Ativar o modulo se houver meio de pagamento selecionado
 
-			if(isset($this->session->data['payment_method']['code']))
+            if(isset($this->session->data['payment_method']['code']))
             {
                 // Looping entre os meios de pagamento configurados
 
                 foreach($metodos_pagamento_array as $value)
                 {
-                	// Validar forma de pagamento
+                    // Validar forma de pagamento
 
-					if($this->session->data['payment_method']['code'] == $value['metodo_pagamento'])
+                    if($this->session->data['payment_method']['code'] == $value['metodo_pagamento'])
                     {
-						// Carrega o Idioma
+                        // Carrega o Idioma
 
-						$this->load->language('total/taxa_por_tipo_pagamento');
+                        $this->load->language('total/taxa_por_tipo_pagamento');
 
                         // Carrega o Model do Cliente
 
@@ -64,7 +64,7 @@ class ModelTotalTaxaPorTipoPagamento extends Model
 
                         // Define o Grupo do Cliente Atual
 
-                        $customer_group_id = isset($customer) ? $customer['customer_group_id'] : 0;
+                        $customer_group_id = empty($customer) ? 0 : $customer['customer_group_id'];
 
                         // Se grupo de clientes é permitido a tdos ou se o Cliente se encaixa no grupo especificado
 
@@ -76,39 +76,39 @@ class ModelTotalTaxaPorTipoPagamento extends Model
 
                             // Se o total do pedido for maior/igual ao minimo e menor/igual ao maximo --ou-- o total do pedido for maior/igual ao minimo e o valor maximo for igual a zero
 
-    						if(($totalComparacao >= $value['valor_minimo'] && $totalComparacao <= $value['valor_maximo']) || ($totalComparacao >= $value['valor_minimo'] && $value['valor_maximo'] == 0))
+                            if(($totalComparacao >= $value['valor_minimo'] && $totalComparacao <= $value['valor_maximo']) || ($totalComparacao >= $value['valor_minimo'] && $value['valor_maximo'] == 0))
                             {
                                 // Calcula a taxa baseado no valor total da compra
 
-    							$valor_taxa_aplicada = $value['tipo_taxa'] == "$" ? $value['taxa'] : $totalComparacao * ($value['taxa']/100);
+                                $valor_taxa_aplicada = $value['tipo_taxa'] == "$" ? $value['taxa'] : $totalComparacao * ($value['taxa']/100);
 
                                 // Texto da taxa aplicada
 
-    							$texto_descricao_taxa_aplicada = $value['tipo_taxa'] == "$" ? $value['descricao']." (".$this->currency->format($valor_taxa_aplicada).")" : $value['descricao']." (".$value['taxa']."%)";
+                                $texto_descricao_taxa_aplicada = $value['tipo_taxa'] == "$" ? $value['descricao']." (".$this->currency->format($valor_taxa_aplicada).")" : $value['descricao']." (".$value['taxa']."%)";
 
                                 // Taxa aplicada formatada
 
-    							$texto_taxa_aplicada = $value['operador'] == '+' ? $this->currency->format($valor_taxa_aplicada) : '- ' . $this->currency->format($valor_taxa_aplicada);
+                                $texto_taxa_aplicada = $value['operador'] == '+' ? $this->currency->format($valor_taxa_aplicada) : '- ' . $this->currency->format($valor_taxa_aplicada);
 
                                 // Array de dados do módulo
 
-    							$total_data[] = array
+                                $total_data[] = array
                                 (
-    								'code'		 => 'taxa_por_tipo_pagamento',
-    								'title'      => $texto_descricao_taxa_aplicada,
-    								'text'       => '<span class="discont">' . $texto_taxa_aplicada . '</span>',
-    								'value'      => $valor_taxa_aplicada,
-    								'sort_order' => $this->config->get('taxa_por_tipo_pagamento_sort_order')
-    							);
+                                    'code'       => 'taxa_por_tipo_pagamento',
+                                    'title'      => $texto_descricao_taxa_aplicada,
+                                    'text'       => '<span class="discont">' . $texto_taxa_aplicada . '</span>',
+                                    'value'      => $valor_taxa_aplicada,
+                                    'sort_order' => $this->config->get('taxa_por_tipo_pagamento_sort_order')
+                                );
 
                                 // Aplica taxa por valor total da compra
 
                                 $total = $value['operador'] == "+" ? ($total + $valor_taxa_aplicada) : ($total - $valor_taxa_aplicada);
-    						}
+                            }
                         }
-					}
-				}
-			}
-		}
-	}
+                    }
+                }
+            }
+        }
+    }
 }
